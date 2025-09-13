98f3fe6a906847600ff493b0bed3c098b30fffa1
Mike Belyaev <pair.dro@gmail.com>
2025-09-13 12:56:36 +0300

    Add file setup and manipulation logic to CLI#install method
    - Add logic to create directory, copy files, and manipulate file contents
      within the CLI#install method.
    - Update tests to verify directory creation, file copying, and content
      manipulation.
    - Ensure proper handling when hook file exists or does not exist.

--- spec/lib/cli_spec.rb
+++ spec/lib/cli_spec.rb
@@ -16,56 +16,67 @@ RSpec.describe AiGitCommit::CLI do
   describe "#install" do
     let(:cli) { described_class.new }
     let(:script_content) { "script content" }
+    let(:file_double) { instance_double(File) }
 
     before do
       allow(cli).to receive(:script).and_return(script_content)
       allow(FileUtils).to receive(:chmod)
+      allow(FileUtils).to receive(:mkdir_p)
+      allow(FileUtils).to receive(:cp)
+      allow(File).to receive(:open).and_yield(file_double)
+      allow(File).to receive(:write)
+      allow(file_double).to receive(:puts)
       allow($stdout).to receive(:puts)
     end
 
     subject { cli.install }
 
-    after { subject }
-
     it "creates a dir and copies initializer file" do
-      expect(File.exist?("config/initializers/ai_git_commit.rb")).to be(true)
+      expect(FileUtils).to receive(:mkdir_p)
+      expect(FileUtils).to receive(:cp)
+      subject
     end
 
     context "when hook file exists" do
       before do
         allow(cli).to receive(:hook_file_exists?).and_return(true)
-        allow(File).to receive(:open)
       end
 
       it "appends script to the hook file" do
-        expect(File).to receive(:open).with(described_class::HOOK_PATH, "a")
+        expect(File).to receive(:open).with(described_class::HOOK_PATH, "a").and_yield(file_double)
+        expect(file_double).to receive(:puts).with(script_content)
+        subject
       end
 
       it "makes the hook file executable" do
         expect(FileUtils).to receive(:chmod).with("+x", described_class::HOOK_PATH)
+        subject
       end
 
       it "prints confirmation message" do
         expect($stdout).to receive(:puts).with("AI Git Commit prepare-commit-msg hook set up.")
+        subject
       end
     end
 
     context "when hook file does not exist" do
       before do
         allow(cli).to receive(:hook_file_exists?).and_return(false)
-        allow(File).to receive(:write)
       end
 
       it "writes script to the hook file" do
         expect(File).to receive(:write).with(described_class::HOOK_PATH, script_content)
+        subject
       end
 
       it "makes the hook file executable" do
         expect(FileUtils).to receive(:chmod).with("+x", described_class::HOOK_PATH)
+        subject
       end
 
       it "prints confirmation message" do
         expect($stdout).to receive(:puts).with("AI Git Commit prepare-commit-msg hook set up.")
+        subject
       end
     end
   end
